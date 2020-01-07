d4c5c97876108a4da8bdbca1ce61d696bd0e79f0
Nesma Almoazamy <nesma.almoazamy93@gmail.com>
2020-01-07 22:51:49 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from classes import *
-from modules import *
+from modules import chess_ai, chess_processing
 
 
 #from classes import GamePosition
@@ -18,15 +18,189 @@ import os #To allow path joining with cross-platform support
 
 # make_move()
 # import make_move from Gamal's code
-from modules.chess_processing import make_move as makemove
+#from modules.chess_processing import make_move as makemove
 
 #pos_to_key()
 # import pos_to_key from Gamal's code
-from modules.chess_processing import pos_to_key as pos2key
+#from modules.chess_processing import pos_to_key as pos2key
 
 # is_check_mate()
 # import isCheckmate from Gamal's code
-from modules.chess_processing import is_check_mate as isCheckmate
+#from modules.chess_processing import is_check_mate as isCheckmate
+
+from modules.chess_processing import *
+from modules.chess_ai import *
+
+
+
+
+##############################////////GUI FUNCTIONS\\\\\\\\\\\\\#############################
+def chess_coord_to_pixels(chess_coord):
+    x,y = chess_coord
+    #There are two sets of coordinates that this function could choose to return.
+    #One is the coordinates that would be usually returned, the other is one that
+    #would be returned if the board were to be flipped.
+    #Note that square width and height variables are defined in the main function and 
+    #so are accessible here as global variables.
+    if isAI:
+        if AIPlayer==0:
+            #This means you're playing against the AI and are playing as black:
+            return ((7-x)*square_width, (7-y)*square_height)
+        else:
+            return (x*square_width, y*square_height)
+#    #Being here means two player game is being played.
+#    #If the flipping mode is enabled, and the player to play is black,
+#    #the board should flip, but not until the transition animation for 
+#    #white movement is complete:
+#    if not isFlip or player==0 ^ isTransition:
+#        return (x*square_width, y*square_height)
+#    else:
+#        return ((7-x)*square_width, (7-y)*square_height)
+def pixel_coord_to_chess(pixel_coord):
+    x,y = pixel_coord[0]/square_width, pixel_coord[1]/square_height
+    #See comments for chess_coord_to_pixels() for an explanation of the
+    #conditions seen here:
+    if isAI:
+        if AIPlayer==0:
+            return (7-x,7-y)
+        else:
+            return (x,y)
+#    if not isFlip or player==0 ^ isTransition:
+#        return (x,y)
+#    else:
+#        return (7-x,7-y)
+def getPiece(chess_coord):
+    for piece in listofWhitePieces+listofBlackPieces:
+        #piece.getInfo()[0] represents the chess coordinate occupied
+        #by piece.
+        if piece.getInfo()[0] == chess_coord:
+            return piece
+def createPieces(board):
+    #Initialize containers:
+    listofWhitePieces = []
+    listofBlackPieces = []
+    #Loop through all squares:
+    for i in range(8):
+        for k in range(8):
+            if board[i][k]!=0:
+                #The square is not empty, create a piece object:
+                p = Piece(board[i][k],(k,i),square_width,square_height)
+                #Append the reference to the object to the appropriate
+                #list:
+                if board[i][k][1]=='w':
+                    listofWhitePieces.append(p)
+                else:
+                    listofBlackPieces.append(p)
+    #Return both:
+    return [listofWhitePieces,listofBlackPieces]
+def createShades(listofTuples):
+    global listofShades
+    #Empty the list
+    listofShades = []
+    if isTransition:
+        #Nothing should be shaded when a piece is being animated:
+        return
+    if isDraw:
+        #The game ended with a draw. Make yellow circle shades for
+        #both the kings to show this is the case:
+        coord = look_for(board,'Kw')[0]
+        shade = Shades(circle_image_yellow,coord)
+        listofShades.append(shade)
+        coord = look_for(board,'Kb')[0]
+        shade = Shades(circle_image_yellow,coord)
+        listofShades.append(shade)
+        #There is no need to go further:
+        return
+    if chessEnded:
+        #The game has ended, with a checkmate because it cannot be a 
+        #draw if the code reached here.
+        #Give the winning king a green circle shade:
+        coord = look_for(board,'K'+winner)[0]
+        shade = Shades(circle_image_green_big,coord)
+        listofShades.append(shade)
+    #If either king is under attack, give them a red circle:
+    if is_check(position,'white'):
+        coord = look_for(board,'Kw')[0]
+        shade = Shades(circle_image_red,coord)
+        listofShades.append(shade)
+    if is_check(position,'black'):
+        coord = look_for(board,'Kb')[0]
+        shade = Shades(circle_image_red,coord)
+        listofShades.append(shade)
+    #Go through all the target squares inputted:
+    for pos in listofTuples:
+        #If the target square is occupied, it can be captured.
+        #For a capturable square, there is a different shade.
+        #Create the appropriate shade for each target square:
+        if is_occupied(board,pos[0],pos[1]):
+            img = circle_image_capture
+        else:
+            img = circle_image_green
+        shade = Shades(img,pos)
+        #Append:
+        listofShades.append(shade)
+def drawBoard():
+    #Blit the background:
+    screen.blit(background,(0,0))
+    #Choose the order in which to blit the pieces.
+    #If black is about to play for example, white pieces
+    #should be blitted first, so that when black is capturing,
+    #the piece appears above:
+    if player==1:
+        order = [listofWhitePieces,listofBlackPieces]
+    else:
+        order = [listofBlackPieces,listofWhitePieces]
+    if isTransition:
+        #If a piece is being animated, the player info is changed despite
+        #white still capturing over black, for example. Reverse the order:
+        order = list(reversed(order))
+    #The shades which appear during the following three conditions need to be
+    #blitted first to appear under the pieces:
+    if isDraw or chessEnded or isAIThink:
+        #Shades
+        for shade in listofShades:
+            img,chess_coord = shade.getInfo()
+            pixel_coord = chess_coord_to_pixels(chess_coord)
+            screen.blit(img,pixel_coord)
+    #Make shades to show what the previous move played was:
+    if prevMove[0]!=-1 and not isTransition:
+        x,y,x2,y2 = prevMove
+        screen.blit(yellowbox_image,chess_coord_to_pixels((x,y)))
+        screen.blit(yellowbox_image,chess_coord_to_pixels((x2,y2)))
+
+    #Blit the Pieces:
+    #Notw that one side has to be below the green circular shades to show
+    #that they are being targeted, and the other side if dragged to such
+    # a square should be blitted on top to show that it is capturing:
+
+    #Potentially captured pieces:
+    for piece in order[0]:
+        
+        chess_coord,subsection,pos = piece.getInfo()
+        pixel_coord = chess_coord_to_pixels(chess_coord)
+        if pos==(-1,-1):
+            #Blit to default square:
+            screen.blit(pieces_image,pixel_coord,subsection)
+        else:
+            #Blit to the specific coordinates:
+            screen.blit(pieces_image,pos,subsection)
+    #Blit the shades in between:
+    if not (isDraw or chessEnded or isAIThink):
+        for shade in listofShades:
+            img,chess_coord = shade.getInfo()
+            pixel_coord = chess_coord_to_pixels(chess_coord)
+            screen.blit(img,pixel_coord)
+    #Potentially capturing pieces:
+    for piece in order[1]:
+        chess_coord,subsection,pos = piece.getInfo()
+        pixel_coord = chess_coord_to_pixels(chess_coord)
+        if pos==(-1,-1):
+            #Default square
+            screen.blit(pieces_image,pixel_coord,subsection)
+        else:
+            #Specifc pixels:
+            screen.blit(pieces_image,pos,subsection)
+
 
 
 #########MAIN FUNCTION####################################################
@@ -118,7 +292,7 @@ screen = pygame.display.set_mode((600,600))
 
 #Load all the images:
 #Load the background chess board image:
-background = pygame.image.load(os.path.join('Media', 'board.jpg')).convert()
+background = pygame.image.load(os.path.join('Media', 'board.png')).convert()
 #Load an image with all the pieces on it:
 pieces_image = pygame.image.load(os.path.join('Media', 'Chess_Pieces_Sprite.png')).convert_alpha()
 circle_image_green = pygame.image.load(os.path.join('Media', 'green_circle_small.png')).convert_alpha()
@@ -237,13 +411,13 @@ while not gameEnded:
             #The user has not selected between playing against the AI
             #or playing against a friend.
             #So allow them to choose between playing with a friend or the AI:
-            screen.blit(withfriend_pic,(0,square_height*2))
+            #screen.blit(withfriend_pic,(0,square_height*2))
             screen.blit(withAI_pic,(square_width*4,square_height*2))
         elif isAI==True:
             #The user has selected to play against the AI.
             #Allow the user to play as white or black:
             screen.blit(playwhite_pic,(0,square_height*2))
-            screen.blit(playblack_pic,(square_width*4,square_height*2))
+            #screen.blit(playblack_pic,(square_width*4,square_height*2))
         elif isAI==False:
             #The user has selected to play with a friend.
             #Allow choice of flipping the board or not flipping the board:
@@ -353,7 +527,7 @@ while not gameEnded:
 
             #If the piece clicked on is not occupied by your own piece,
             #ignore this mouse click:
-            if not isOccupiedby(board,x,y,'wb'[player]):
+            if not is_occupied_by(board,x,y,'wb'[player]):
                 continue
             #Now we're sure the user is holding their mouse on a
             #piecec that is theirs.
@@ -361,14 +535,14 @@ while not gameEnded:
             dragPiece = getPiece(chess_coord)
             print(dragPiece)
             #Find the possible squares that this piece could attack:
-            listofTuples = findPossibleSquares(position,x,y)
+            listofTuples = find_possible_squares(position,x,y)
             #Highlight all such squares:
             createShades(listofTuples)
             #A green box should appear on the square which was selected, unless
             #it's a king under check, in which case it shouldn't because the king
             #has a red color on it in that case.
             if ((dragPiece.pieceinfo[0]=='K') and
-                (isCheck(position,'white') or isCheck(position,'black'))):
+                (is_check(position,'white') or is_check(position,'black'))):
                 None
             else:
                 listofShades.append(Shades(greenbox_image,(x,y)))
@@ -403,7 +577,7 @@ while not gameEnded:
                         createShades([])
                     else:
                         #User clicked elsewhere on this second click:
-                        if isOccupiedby(board,x2,y2,'wb'[player]):
+                        if is_occupied_by(board,x2,y2,'wb'[player]):
                             #User clicked on a square that is occupied by their
                             #own piece.
                             #This is like making a first click on your own piece:
@@ -430,7 +604,7 @@ while not gameEnded:
                     openings[key].append([(x,y),(x2,y2)])
 
             #Make the move:
-            makemove(position,x,y,x2,y2)
+            make_move(position,x,y,x2,y2)
             #Update this move to be the 'previous' move (latest move in fact), so that
             #yellow shades can be shown on it.
             prevMove = [x,y,x2,y2]
@@ -440,15 +614,15 @@ while not gameEnded:
             position.addtoHistory(position)
             #Check for possibilty of draw:
             HMC = position.getHMC()
-            if HMC>=100 or isStalemate(position) or position.checkRepition():
+            if HMC>=100 or is_stalemate(position) or position.checkRepition():
                 #There is a draw:
                 isDraw = True
                 chessEnded = True
             #Check for possibilty of checkmate:
-            if isCheckmate(position,'white'):
+            if is_check_mate(position,'white'):
                 winner = 'b'
                 chessEnded = True
-            if isCheckmate(position,'black'):
+            if is_check_mate(position,'black'):
                 winner = 'w'
                 chessEnded = True
             #If the AI option was selecteed and the game still hasn't finished,
@@ -513,7 +687,7 @@ while not gameEnded:
             #Get the move proposed:
             [x,y],[x2,y2] = bestMoveReturn
             #Do everything just as if the user made a move by click-click movement:
-            makemove(position,x,y,x2,y2)
+            make_move(position,x,y,x2,y2)
             prevMove = [x,y,x2,y2]
             player = position.getplayer()
             HMC = position.getHMC()
